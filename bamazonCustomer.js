e600925d97ff4664a60393528f2139d905580128
Krishna <kaddala@salesforce.com>
2019-12-12 19:28:24 -0800

    adding console log table and wrapping them in functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,32 @@
 var mysql = require("mysql");
-var inquirer = require ("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "password",
-  database: "bamazonDB"
+    // Your password
+    password: "password",
+    database: "bamazonDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  //connection.end();
-});
-connection.query(function(err, res){
+connection.connect(function (err) {
     if (err) throw err;
-  console.table(res);
-})
+    console.log("connected as id " + connection.threadId);
+    //connection.end();
+    lookupTable();
+});
+
+function lookupTable() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+
