9895de708f332c32b2f4597608714a80ab30a5c4
Krishna <kaddala@salesforce.com>
2019-12-17 20:54:04 -0800

    logic for udpate statement and adding callback functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,58 +31,65 @@ function lookupTable() {
 }
 
 function checkInventory(item, quantity, inventory) {
-    console.log(item + "||" + quantity + "||");
-    // console.table(inventory);
-    console.log(JSON.stringify(inventory, null, 2));
-    //create a function with a multiple IF n ELSE
-    // we should check if how many of the chosen items are in the DB (SQL statement)
+    //console.log(JSON.stringify(inventory, null, 2));
     for (i = 0; i < inventory.length; i++) {
-        if (inventory[i].item_id === item) {
+        if (inventory[i].item_id == item) {
+            // if the user chosen quantity is > stock quantity then console.log ("insufficient quantity")
             if (quantity > inventory[i].stock_quantity) {
-                console.log("insufficient quantity");
+                console.log("Insufficient Quantity!");
+                // if they ask too much call the function which inquires the user from the start on what they 
+                //want to choose from the list of items.
+                userPrompts(inventory);
+            }
+            else {
+                var purchasePrice = parseInt(inventory[i].price) * quantity;
+                console.log(purchasePrice);
+                var product = inventory[i].product_name;
+                // else decrement the quantity in the DB, mySQL statement UPDATE to decrement the stock_quantity
+                connection.query("UPDATE products SET stock_quantity = stock_quantity-? WHERE item_id = ?", [quantity, item], function (err, res) {
+                    if (err) throw err;
+                    console.log("You have successfully made your purchase! " + product + "\n" + " & it costed you " + purchasePrice);
+                    lookupTable();
+                });
             }
         }
     }
 
-    // if the user chosen quantity is > stock quantity then console.log ("insufficient quantity")
-    // if they ask too much call the function which inquires the user from the start on what they 
-    //want to choose from the list of items.
-    // else decrement the quantity in the DB, mySQL statement UPDATE to decrement the stock_quantity
 }
 
 function userPrompts(inventory) {
-    inquirer.prompt([
+
+    inquirer.prompt(
         {
             type: "input",
             name: "item",
-            message: "What Item do you want to purchase? Please enter the ID of purchase: "
-        },
-        {
-            type: "input",
-            name: "quantity",
-            message: "How many do you want to purchase?"
-        },
-        // After the prompt, store the user's response in a variable called location.
-    ]).then(function (userResponse) {
-        if (userResponse === "Q") {
+            message: "What Item do you want to purchase? Please enter the ID of purchase: [Press Q to Quit]"
+        }
+    ).then(function (userResponse1) {
+        if (userResponse1.item.toLowerCase() === "q") {
             console.log("Thanks for visiting our store!");
             connection.end();
         }
         else {
-            //create a function with a multiple IF n ELSE
-            checkInventory(userResponse.item, userResponse.quantity, inventory);
-            // we should check if how many of the chosen items are in the DB (SQL statement)
-            // if the user chosen quantity is > stock quantity then console.log ("insufficient quantity")
-            // if they ask too much call the function which inquires the user from the start on what they 
-            //want to choose from the list of items.
-            // else decrement the quantity in the DB, mySQL statement UPDATE to decrement the stock_quantity
-            //
+            inquirer.prompt(
+                {
+                    type: "input",
+                    name: "quantity",
+                    message: "How many do you want to purchase? [Press Q to Quit]"
+                }
+            ).then(function (userResponse2) {
+                if (userResponse.quantity.toLowerCase() === "q") {
+                    console.log("Thanks for visiting our store!");
+                    connection.end();
+                }
+                else {
+                    //create a function with a multiple IF n ELSE
+                    checkInventory(userResponse.item, userResponse.quantity, inventory);
+                }
+
+            });
         }
-        // console.log(location.userInput);
-        //     // Then use the Google Geocoder to Geocode the address
-        //     geocoder.geocode(location.userInput, function(err, data) {
-        //       console.log(JSON.stringify(data, null, 2));
-        //     });
-    });
-}
+    })
+};
+
 
